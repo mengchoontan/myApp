f6caf39b91fa7525027b6f509db89702dc7f36f9
admin <mc@dapoltd.com>
2016-05-04 13:06:56 +0800

    Added implementation on File choose from local device

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,9 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $pouchDB) {
+
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -24,7 +26,158 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       console.log("The application is resuming from the background");
     }, false);
 
+    $pouchDB.setDatabase("dbLocal");
+    if(ionic.Platform.isAndroid()) {
+        // $pouchDB.sync("http://192.168.57.1:4984/test-database");
+    } else {
+        // $pouchDB.sync("http://localhost:4984/test-database");
+    }
+
   });
+
+
+
+
+
+})
+
+
+.service("$pouchDB", ["$rootScope", "$q", function($rootScope, $q) {
+
+    var database;
+    var changeListener;
+
+    this.setDatabase = function(databaseName) {
+        database = new PouchDB(databaseName);
+    }
+
+    this.startListening = function() {
+        changeListener = database.changes({
+            live: true,
+            include_docs: true
+        }).on("change", function(change) {
+            if(!change.deleted) {
+                $rootScope.$broadcast("$pouchDB:change", change);
+            } else {
+                $rootScope.$broadcast("$pouchDB:delete", change);
+            }
+        });
+    }
+
+    this.stopListening = function() {
+        changeListener.cancel();
+    }
+
+    this.sync = function(remoteDatabase) {
+        database.sync(remoteDatabase, {live: true, retry: true});
+    }
+
+    this.save = function(jsonDocument) {
+
+        console.log("pouchDB save " +  JSON.stringify( jsonDocument, null, 2));
+
+        var deferred = $q.defer();
+        if(!jsonDocument._id) {
+            database.post(jsonDocument).then(function(response) {
+                deferred.resolve(response);
+            }).catch(function(error) {
+                deferred.reject(error);
+            });
+        } else {
+            database.put(jsonDocument).then(function(response) {
+                deferred.resolve(response);
+            }).catch(function(error) {
+                deferred.reject(error);
+            });
+        }
+        return deferred.promise;
+    }
+
+    this.delete = function(documentId, documentRevision) {
+        return database.remove(documentId, documentRevision);
+    }
+
+    this.get = function(documentId) {
+        return database.get(documentId);
+    }
+
+    this.destroy = function() {
+        database.destroy();
+    }
+
+}])
+
+.factory("$fileFactory", function($q) {
+
+    var File = function() { };
+
+    File.prototype = {
+
+
+        getParentDirectory: function(path) {
+            var deferred = $q.defer();
+            window.resolveLocalFileSystemURI(path, function(fileSystem) {
+                fileSystem.getParent(function(result) {
+                    deferred.resolve(result);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntriesAtRoot: function() {
+            var deferred = $q.defer();
+
+    console.log("fileFactory dataDir " + cordova.file.externalRootDirectory);
+
+            //window.requestFileSystem(cordova.file.externalRootDirectory, 0, function(fileSystem) {
+
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+
+                console.log("fileFactory getEntriesAtRoot fileSystem" +  JSON.stringify( fileSystem, null, 2));
+                var directoryReader = fileSystem.root.createReader();
+
+                console.log("fileFactory getEntriesAtRoot" +  JSON.stringify(directoryReader, null, 2));
+                directoryReader.readEntries(function(entries) {
+
+                    console.log("fileFactory getEntriesAtRoot entries" +  JSON.stringify(entries, null, 2));
+                    deferred.resolve(entries);
+                }, function(error) {
+                    deferred.reject(error);
+		    console.log(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntries: function(path) {
+            var deferred = $q.defer();
+
+	    console.log("getEntries " + path);
+            window.resolveLocalFileSystemURI(path, function(fileSystem) {
+                var directoryReader = fileSystem.createReader();
+                directoryReader.readEntries(function(entries) {
+                    deferred.resolve(entries);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        }
+
+    };
+
+   console.log("fileFactory");
+
+    return File;
+
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -60,7 +213,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       url: '/browse',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/browse.html',
+          controller: 'BrowseCtrl'
+        }
+      }
+    })
+  .state('app.filechoose', {
+      url: '/filechoose',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/filechoose.html',
+          controller: 'FileCtrl'
         }
       }
     })
@@ -74,6 +237,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
+  .state("list", {
+            "url": "/list",
+            "templateUrl": "templates/list.html",
+            "controller": "FileCtrl"
+        })
+        .state("item", {
+            "url": "/item/:documentId/:documentRevision",
+            "templateUrl": "templates/item.html",
+            "controller": "FileCtrl"
+   })
+
   .state('app.single', {
     url: '/playlists/:playlistId',
     views: {
@@ -85,4 +259,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
+  // $urlRouterProvider.otherwise('/list');
+
+
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -51,36 +51,154 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     { title: 'Cowbell', id: 6 }
   ];
 })
-.controller('ConfigCtrl', function($scope, $timeout, $cordovaFileTransfer) {
+.controller('ConfigCtrl', function($scope, $timeout, $cordovaFileTransfer, $ionicPlatform, $fileFactory) {
+
+   var fs = new $fileFactory();
+
+
+   $ionicPlatform.ready(function() {
+
+   fs.getEntriesAtRoot().then(function(result) {
+            $scope.files = result;
+            console.log("getRoot" +  JSON.stringify($scope.files, null, 2) );
+        }, function(error) {
+            console.error(error);
+   })
+   ;
 
    $scope.upload = function() {
 
     var url = "http://ionicframework.com/img/ionic-logo-blog.png";
-    var targetPath = cordova.file.externalRootDirectory + "auguri.png";
+    var targetPath = cordova.file.documentsDirectory + "auguri.png";
     var trustHosts = true;
     var options = {};
 
     //if($cordovaDevice.getPlatform() == 'iOS'){
     if( ionic.Platform.isAndroid() ){
-       targetPath = cordova.file.externalRootDirectory + "auguri.png";
+       targetPath = cordova.file.documentsDirectory + "Aauguri.png";
     }else{
-       targetPath = cordova.file.dataDirectory + "auguri.png";
+       //targetPath = cordova.file.dataDirectory + "auguri.png";
+       targetPath = cordova.file.documentsDirectory + "Iauguri.png";
     }
+   console.log("upload > " + targetPath); 
 
-    $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+   $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
       .then(function(result) {
         // Success!
-         console.log('Success' + targetPath);
+         console.log('Success' , targetPath);
       }, function(err) {
         // Error
-         console.log('Error' + targetPath);
+         console.log('Error' , targetPath);
       }, function (progress) {
         $timeout(function () {
           $scope.downloadProgress = (progress.loaded / progress.total) * 100;
         });
       });
-   alert("upload > " + targetPath); 
+      alert("upload > " + targetPath);
     }
+
+   $scope.getContents = function(path) {
+
+	    alert("Path " + path);
+            fs.getEntries(path).then(function(result) {
+                $scope.files = result;
+                $scope.files.unshift({name: "[parent]"});
+                fs.getParentDirectory(path).then(function(result) {
+                    result.name = "[parent]";
+                    $scope.files[0] = result;
+                });
+            });
+
+
+   }
+
+ });
+
+})
+
+.controller('BrowseCtrl0', function($scope, $stateParams) {
+   alert("BrowseCtrl");
+})
+
+.controller('FileCtrl', function($scope, $stateParams, $rootScope, $state, $ionicHistory, $pouchDB ) {
+
+   console.log("FileCtrl");
+
+   $scope.items = [
+    { "firstname": "filename", "lastname": "lastname", "email": "email" },
+    { "firstname": "filename", "lastname": "lastname", "email": "email" }
+
+   ];
+   
+   $scope.filepathChooser = function() {
+     window.plugins.mfilechooser.open([], function (uri) {
+       //Here uri provides the selected file path.
+      console.log('file path', uri);
+      alert(uri);
+      $scope.save(uri, 'lastname', 'email');
+      }, function (error) {
+        console.log('Error', error);
+     alert(error);
+    });
+   };
+
+   $scope.save = function(firstname, lastname, email) {
+    console.log('Saving ', firstname);
+    var jsonDocument = {
+        "firstname": firstname,
+        "lastname": lastname,
+        "email": email
+    };
+    if($stateParams.documentId) {
+        jsonDocument["_id"] = $stateParams.documentId;
+        jsonDocument["_rev"] = $stateParams.documentRevision;
+        console.log('documentId ',  jsonDocument["_id"]);
+    }
+    $pouchDB.save(jsonDocument).then(function(response) {
+        //$state.go("app.filechoose");
+        $state.go("list");
+    }, function(error) {
+        console.log("ERROR -> " + error);
+    });
+   };
+
+})
+
+.controller("BrowseCtrl", function($scope, $ionicPlatform, $fileFactory) {
+
+    var fs = new $fileFactory();
+
+   $scope.playlists = [
+    { name: 'Reggae', id: 1 },
+    { name: 'Chill', id: 2 },
+    { name: 'Dubstep', id: 3 },
+    { name: 'Cowbell', id: 6 }
+   ];
+
+    $ionicPlatform.ready(function() {
+        fs.getEntriesAtRoot().then(function(result) {
+
+            console.log("BrowseCtrl - fs.getEntriesAtRoot Filelist >>" + JSON.stringify(result, null, 4));
+            $scope.files = result;
+        }, function(error) {
+            console.error(error);
+            console.log(error);
+        });
+
+
+        $scope.getContents = function(path) {
+	    console.log("Browse path " + path);
+            fs.getEntries(path).then(function(result) {
+                $scope.files = result;
+                $scope.files.unshift({name: "[parent]"});
+                fs.getParentDirectory(path).then(function(result) {
+                    result.name = "[parent]";
+                    $scope.files[0] = result;
+                });
+            });
+        }
+    });
+
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
