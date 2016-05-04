1aa817c83b73bd20ea67f4f5a3fca275da8eb79c
MC <mc@dapoltd.com>
2016-05-04 13:21:36 +0800

    Implement local DB sync and persistence

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,11 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCsvImport'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $pouchDB) {
+
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -24,7 +26,352 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       console.log("The application is resuming from the background");
     }, false);
 
+    $pouchDB.setDatabase("nraboy-test");
+    if(ionic.Platform.isAndroid()) {
+         $pouchDB.sync("http://192.168.57.1:4984/test-database");
+    } else {
+         $pouchDB.sync("http://localhost:4984/test-database");
+    }
+
   });
+
+
+
+
+
+})
+
+.service("$pouchDB", ["$rootScope", "$q", function($rootScope, $q) {
+
+    var database;
+    var changeListener;
+
+    this.setDatabase = function(databaseName) {
+        database = new PouchDB(databaseName);
+    }
+
+    this.startListening = function() {
+        changeListener = database.changes({
+            live: true,
+            include_docs: true
+        }).on("change", function(change) {
+	    console.log("database change " +  JSON.stringify( change, null, 2));
+            if(!change.deleted) {
+                $rootScope.$broadcast("$pouchDB:change", change);
+            } else {
+                $rootScope.$broadcast("$pouchDB:delete", change);
+            }
+        });
+    }
+
+    this.stopListening = function() {
+        changeListener.cancel();
+    }
+
+    this.sync = function(remoteDatabase) {
+        database.sync(remoteDatabase, {live: true, retry: true});
+    }
+
+    this.save = function(jsonDocument) {
+
+        console.log("pouchDB save " +  JSON.stringify( jsonDocument, null, 2));
+
+        var deferred = $q.defer();
+        if(!jsonDocument._id) {
+	    console.log("database posting " +  JSON.stringify( jsonDocument, null, 2));
+
+            database.post(jsonDocument).then(function(response) {
+	        console.log("database response " +  JSON.stringify( response, null, 2));
+                deferred.resolve(response);
+            }).catch(function(error) {
+                deferred.reject(error);
+            });
+        } else {
+            database.put(jsonDocument).then(function(response) {
+                deferred.resolve(response);
+            }).catch(function(error) {
+                deferred.reject(error);
+            });
+        }
+        return deferred.promise;
+    }
+
+    this.delete = function(documentId, documentRevision) {
+        return database.remove(documentId, documentRevision);
+    }
+
+    this.get = function(documentId) {
+        return database.get(documentId);
+    }
+
+    this.destroy = function() {
+        database.destroy();
+    }
+
+}])
+
+.factory("$fileFactory", function($q, $http, $pouchDB) {
+
+    var File = function() { };
+
+    File.prototype = {
+
+
+        getParentDirectory: function(path) {
+            var deferred = $q.defer();
+            //window.resolveLocalFileSystemURI(path, function(fileSystem) {
+            window.resolveLocalFileSystemURL(path, function(fileSystem) {
+                fileSystem.getParent(function(result) {
+                    deferred.resolve(result);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntriesAtRoot: function() {
+            var deferred = $q.defer();
+
+            //console.log("fileFactory dataDir " + cordova.file.externalRootDirectory);
+
+            //window.requestFileSystem(cordova.file.externalRootDirectory, 0, function(fileSystem) {
+
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+
+                //console.log("fileFactory getEntriesAtRoot fileSystem" +  JSON.stringify( fileSystem, null, 2));
+                var directoryReader = fileSystem.root.createReader();
+
+                //console.log("fileFactory getEntriesAtRoot" +  JSON.stringify(directoryReader, null, 2));
+                directoryReader.readEntries(function(entries) {
+
+                    console.log("fileFactory getEntriesAtRoot entries" +  JSON.stringify(entries, null, 2));
+                    deferred.resolve(entries);
+                }, function(error) {
+                    deferred.reject(error);
+		    console.log(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntries: function(path) {
+            var deferred = $q.defer();
+
+	    console.log("getEntries " + path);
+
+            //window.resolveLocalFileSystemURI(path, function(fileSystem) {
+            window.resolveLocalFileSystemURL(path, function(fileSystem) {
+		console.log("FileSystem checked ", JSON.stringify(fileSystem, null, 2));
+		if (fileSystem.isDirectory)  {
+
+                   var directoryReader = fileSystem.createReader();
+                   directoryReader.readEntries(function(entries) {
+                       deferred.resolve(entries);
+                   }, function(error) {
+                       deferred.reject(error);
+                   });
+
+		} else {
+		   //console.log("Path ", JSON.stringify(fileSystem, null, 2));
+		   console.log("Path ", fileSystem.nativeURL);
+
+		   fileSystem.file(function(file) {
+		       var reader = new FileReader();
+			reader.onloadend = function(e) {
+				
+				//console.log("INPUT " , this.result);	
+				var csv_array = processData(this.result);
+				//console.log("INPUT " , csv_array);	
+				var json = CSV2JSON(this.result);
+
+				for (var key in json) {
+				  if (json.hasOwnProperty(key)) {
+				    var val = json[key];
+				    console.log("record " + key + " = " + val);
+
+
+				    
+			        $pouchDB.save(val).then(function(response) {
+            				//$state.go("app.todo");
+			        }, function(error) {
+			            console.log("ERROR -> " + error);
+			        });
+
+
+				  }
+				}	
+	 	   		//console.log("Reading contents ", JSON.stringify(json, null, 2));
+				console.log("Uploading to DB ", json);
+/*
+			        $pouchDB.save(json).then(function(response) {
+            				//$state.go("app.todo");
+			        }, function(error) {
+			            console.log("ERROR -> " + error);
+			        });
+*/
+
+
+            			//document.querySelector("#textArea").innerHTML = this.result;
+            		}
+			reader.readAsText(file);
+			//reader.readAsCSV(file);
+		   });
+
+
+		   // using http.get example
+		   // $http.get(fileSystem.fullPath)
+        // 		.success(function (data) {
+          //   			// The json data will now be in scope.
+            // 			$scope.myJsonData = data;
+	// 	   		console.log("Reading contents ", JSON.stringify(data, null, 2));
+        // 	   })
+	// 	   .error(function (data) {
+	// 	      console.log("error reading");
+	// 	   });
+
+
+		   //alert("Path ", JSON.stringify(fileSystem, null, 2));
+
+		   function processData(csv) {
+                       var allTextLines = csv.split(/\r\n|\n/);
+                       var lines = [];
+                       for (var i=0; i<allTextLines.length; i++) {
+                           var data = allTextLines[i].split(';');
+                               var tarr = [];
+                               for (var j=0; j<data.length; j++) {
+                                   tarr.push(data[j]);
+                               }
+                               lines.push(tarr);
+                       }
+                    // console.log(lines);
+		       return (lines);
+		     
+                   };
+
+		   function CSVToArray( strData, strDelimiter ){
+                       // Check to see if the delimiter is defined. If not,
+                       // then default to comma.
+                       strDelimiter = (strDelimiter || ",");
+
+                       // Create a regular expression to parse the CSV values.
+                       var objPattern = new RegExp(
+                           (
+                               // Delimiters.
+                               "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+                               // Quoted fields.
+                               "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+                               // Standard fields.
+                               "([^\"\\" + strDelimiter + "\\r\\n]*))"
+                           ),
+                           "gi"
+                           );
+
+
+                       // Create an array to hold our data. Give the array
+                       // a default empty first row.
+                       var arrData = [[]];
+
+                       // Create an array to hold our individual pattern
+                       // matching groups.
+                       var arrMatches = null;
+
+
+                       // Keep looping over the regular expression matches
+                       // until we can no longer find a match.
+                       while (arrMatches = objPattern.exec( strData )){
+
+                           // Get the delimiter that was found.
+                           var strMatchedDelimiter = arrMatches[ 1 ];
+
+                           // Check to see if the given delimiter has a length
+                           // (is not the start of string) and if it matches
+                           // field delimiter. If id does not, then we know
+                           // that this delimiter is a row delimiter.
+                           if (
+                               strMatchedDelimiter.length &&
+                               strMatchedDelimiter !== strDelimiter
+                               ){
+
+                               // Since we have reached a new row of data,
+                               // add an empty row to our data array.
+                               arrData.push( [] );
+
+                           }
+
+                           var strMatchedValue;
+
+                           // Now that we have our delimiter out of the way,
+                           // let's check to see which kind of value we
+                           // captured (quoted or unquoted).
+                           if (arrMatches[ 2 ]){
+
+                               // We found a quoted value. When we capture
+                               // this value, unescape any double quotes.
+                               strMatchedValue = arrMatches[ 2 ].replace(
+                                   new RegExp( "\"\"", "g" ),
+                                   "\""
+                                   );
+               
+                           } else {
+
+                               // We found a non-quoted value.
+                               strMatchedValue = arrMatches[ 3 ];
+
+                           }
+
+
+                           // Now that we have our value string, let's add
+                           // it to the data array.
+                           arrData[ arrData.length - 1 ].push( strMatchedValue );
+                       }
+
+                       // Return the parsed data.
+                       return( arrData );
+
+                   } // processData
+
+
+                   function CSV2JSON(csv) {
+                       var array = CSVToArray(csv);
+                       var objArray = [];
+                       for (var i = 1; i < array.length; i++) {
+                           objArray[i - 1] = {};
+                           for (var k = 0; k < array[0].length && k < array[i].length; k++) {
+                               var key = array[0][k];
+                               objArray[i - 1][key] = array[i][k]
+                           }
+                       }
+
+                       var json = JSON.stringify(objArray);
+                       var str = json.replace(/},/g, "},\r\n");
+
+                       //return str;
+                       return objArray;
+                   } // CSV2JSON
+
+
+		}
+			
+            }, function(error) {
+	        console.log("gtiveURLetEntries file? " + path);
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        }
+
+    };
+
+   console.log("fileFactory");
+
+   return File;
+
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -60,7 +407,35 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       url: '/browse',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/browse.html',
+          controller: 'ImportCtrl'
+        }
+      }
+    })
+  .state('app.filechoose', {
+      url: '/filechoose',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/filechoose.html',
+          controller: 'FileCtrl'
+        }
+      }
+    })
+  .state('app.todo', {
+      url: '/todo',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/todo.html',
+          controller: 'TodoCtrl'
+        }
+      }
+    })
+  .state('app.item', {
+      url: '/item/:documentId/:documentRevision',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/item.html',
+          controller: 'TodoCtrl'
         }
       }
     })
@@ -74,6 +449,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
+  .state("list", {
+            "url": "/list",
+            "templateUrl": "templates/list.html",
+            "controller": "FileCtrl"
+        })
+   .state("item", {
+            "url": "/item/:documentId/:documentRevision",
+            "templateUrl": "templates/item.html",
+            "controller": "FileCtrl"
+   })
+
   .state('app.single', {
     url: '/playlists/:playlistId',
     views: {
@@ -85,4 +471,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
+  // $urlRouterProvider.otherwise('/list');
+
+
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -51,36 +51,261 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     { title: 'Cowbell', id: 6 }
   ];
 })
-.controller('ConfigCtrl', function($scope, $timeout, $cordovaFileTransfer) {
+.controller('ConfigCtrl', function($scope, $timeout, $cordovaFileTransfer, $ionicPlatform, $fileFactory, $state) {
+
+   var fs = new $fileFactory();
+
+
+   $ionicPlatform.ready(function() {
+
+   fs.getEntriesAtRoot().then(function(result) {
+            $scope.files = result;
+            console.log("getRoot" +  JSON.stringify($scope.files, null, 2) );
+        }, function(error) {
+            console.error(error);
+   })
+   ;
 
    $scope.upload = function() {
 
     var url = "http://ionicframework.com/img/ionic-logo-blog.png";
-    var targetPath = cordova.file.externalRootDirectory + "auguri.png";
+    var targetPath = cordova.file.documentsDirectory + "auguri.png";
     var trustHosts = true;
     var options = {};
 
     //if($cordovaDevice.getPlatform() == 'iOS'){
     if( ionic.Platform.isAndroid() ){
-       targetPath = cordova.file.externalRootDirectory + "auguri.png";
+       targetPath = cordova.file.documentsDirectory + "Aauguri.png";
     }else{
-       targetPath = cordova.file.dataDirectory + "auguri.png";
+       //targetPath = cordova.file.dataDirectory + "auguri.png";
+       targetPath = cordova.file.documentsDirectory + "Iauguri.png";
     }
+   console.log("upload > " + targetPath); 
 
-    $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+   $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
       .then(function(result) {
         // Success!
-         console.log('Success' + targetPath);
+         console.log('Success' , targetPath);
       }, function(err) {
         // Error
-         console.log('Error' + targetPath);
+         console.log('Error' , targetPath);
       }, function (progress) {
         $timeout(function () {
           $scope.downloadProgress = (progress.loaded / progress.total) * 100;
         });
       });
-   alert("upload > " + targetPath); 
+      alert("upload > " + targetPath);
     }
+
+   $scope.getContents = function(path) {
+
+            fs.getEntries(path).then(function(result) {
+                $scope.files = result;
+                $scope.files.unshift({name: "[parent]"});
+                fs.getParentDirectory(path).then(function(result) {
+                    result.name = "[parent]";
+                    $scope.files[0] = result;
+                }) 
+		// ,function (path) {
+		//    console.log("path " , path);
+		//}
+                
+                ;
+		// Recursively go into the directory
+		$state.go("app.config");
+            });
+	    //alert("Path ", path);
+
+   }
+
+ });
+
+})
+
+.controller('ImportCtrl', function($scope, $parse, $stateParams) {
+   console.log("ImportCtrl");
+
+   angular.element(document).find('input')[0].addEventListener('change', function (e) {
+   console.log("onChange");
+
+   }, false);
+
+   $scope.csv = {
+    	content: null,
+    	header: true,
+    	headerVisible: true,
+    	separator: ',',
+    	separatorVisible: true,
+    	result: null,
+    	encoding: 'ISO-8859-1',
+    	encodingVisible: true,
+    };
+
+    var _lastGoodResult = '';
+
+    $scope.toPrettyJSON = function (json, tabWidth) {
+	var objStr = JSON.stringify(json);
+	var obj = null;
+
+        console.log("ImportCtrl >>" + JSON.stringify(json, null, 2));
+	try {
+		obj = $parse(objStr)({});
+	} catch(e){
+  	// eat $parse error
+	   return _lastGoodResult;
+	}
+
+	var result = JSON.stringify(obj, null, Number(tabWidth));
+	_lastGoodResult = result;
+
+	return result;
+    };
+
+   $scope.display = function () {
+
+	console.log("Display CSV ");
+   }
+
+})
+
+.controller('FileCtrl', function($scope, $stateParams, $rootScope, $state, $ionicHistory, $pouchDB ) {
+
+   console.log("FileCtrl");
+
+   $scope.items = [
+    { "firstname": "filename", "lastname": "lastname", "email": "email" },
+    { "firstname": "filename", "lastname": "lastname", "email": "email" }
+
+   ];
+   
+   $scope.filepathChooser = function() {
+     window.plugins.mfilechooser.open([], function (uri) {
+       //Here uri provides the selected file path.
+      console.log('file path', uri);
+      alert(uri);
+      $scope.save(uri, 'lastname', 'email');
+      }, function (error) {
+        console.log('Error', error);
+     alert(error);
+    });
+   };
+
+   $scope.save = function(firstname, lastname, email) {
+    console.log('Saving ', firstname);
+    var jsonDocument = {
+        "SN": firstname,
+        "Name": lastname,
+        "Gender": email
+    };
+    if($stateParams.documentId) {
+        jsonDocument["_id"] = $stateParams.documentId;
+        jsonDocument["_rev"] = $stateParams.documentRevision;
+        console.log('documentId ',  jsonDocument["_id"]);
+    }
+    $pouchDB.save(jsonDocument).then(function(response) {
+        //$state.go("app.filechoose");
+        //$state.go("list");
+        $state.go("app.todo");
+    }, function(error) {
+        console.log("ERROR -> " + error);
+    });
+   };
+
+})
+
+.controller("BrowseCtrl", function($scope, $ionicPlatform, $fileFactory) {
+
+    var fs = new $fileFactory();
+
+   $scope.playlists = [
+    { name: 'Reggae', id: 1 },
+    { name: 'Chill', id: 2 },
+    { name: 'Dubstep', id: 3 },
+    { name: 'Cowbell', id: 6 }
+   ];
+
+    $ionicPlatform.ready(function() {
+        fs.getEntriesAtRoot().then(function(result) {
+
+            console.log("BrowseCtrl - fs.getEntriesAtRoot Filelist >>" + JSON.stringify(result, null, 4));
+            $scope.files = result;
+        }, function(error) {
+            console.error(error);
+            console.log(error);
+        });
+
+
+        $scope.getContents = function(path) {
+	    console.log("Browse path " + path);
+            fs.getEntries(path).then(function(result) {
+                $scope.files = result;
+                $scope.files.unshift({name: "[parent]"});
+                fs.getParentDirectory(path).then(function(result) {
+                    result.name = "[parent]";
+                    $scope.files[0] = result;
+                });
+            });
+        }
+    });
+
+})
+
+.controller("TodoCtrl", function($scope, $rootScope, $state, $stateParams, $ionicHistory, $pouchDB) {
+
+    $scope.items = {};
+
+    $pouchDB.startListening();
+
+    $rootScope.$on("$pouchDB:change", function(event, data) {
+        $scope.items[data.doc._id] = data.doc;
+        $scope.$apply();
+        //console.log("items loading " +  JSON.stringify( data.doc, null, 2));
+        //console.log("items loading " +  JSON.stringify( $scope.items, null, 2));
+    });
+
+    $rootScope.$on("$pouchDB:delete", function(event, data) {
+        delete $scope.items[data.doc._id];
+        $scope.$apply();
+    });
+
+    if($stateParams.documentId) {
+        $pouchDB.get($stateParams.documentId).then(function(result) {
+            $scope.inputForm = result;
+        });
+    }
+
+    $scope.save = function(firstname, lastname, email) {
+        var jsonDocument = {
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email
+        };
+        if($stateParams.documentId) {
+            jsonDocument["_id"] = $stateParams.documentId;
+            jsonDocument["_rev"] = $stateParams.documentRevision;
+        }
+        $pouchDB.save(jsonDocument).then(function(response) {
+            //$state.go("list");
+            $state.go("app.todo");
+        }, function(error) {
+            console.log("ERROR -> " + error);
+        });
+    }
+
+    $scope.delete = function(id, rev) {
+        $pouchDB.delete(id, rev);
+    }
+
+    $scope.back = function() {
+        $ionicHistory.goBack();
+    }
+    $scope.destroy = function() {
+        console.log("drop database");
+        alert("drop database");
+        $pouchDB.destroy();
+        $scope.items = {};
+    }
+
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
